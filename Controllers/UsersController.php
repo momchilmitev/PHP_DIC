22b5d8b7e1d62a02ff4875a24866ca51b68a18ab
momchilmitev <momchilmitev87@gmail.com>
2020-11-24 17:45:29 +0100

    added the view as private property to the controller with constructor

--- Controllers/UsersController.php
+++ Controllers/UsersController.php
@@ -7,15 +7,27 @@ use ViewEngine\ViewInterface;
 
 class UsersController
 {
+    private ViewInterface $view;
+
+    /**
+     * UsersController constructor.
+     * @param ViewInterface $view
+     */
+    public function __construct(ViewInterface $view)
+    {
+        $this->view = $view;
+    }
+
+
     public function register()
     {
         echo "this is register method";
     }
 
-    public function login(int $id, string $name, ViewInterface $view)
+    public function login(int $id, string $name)
     {
         $viewModel = new UserLoginViewModel($id, $name);
-        $view->render($viewModel);
+        $this->view->render($viewModel);
     }
 
     public function profile()
